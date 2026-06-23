f32e84518c5593153ada2fa96e20ba20a42d8546
negar <niazadeh.negar@gmail.com>
2026-06-23 21:09:32 +0330

    added logo and link it to home page

--- src/Components/PageNave.jsx
+++ src/Components/PageNave.jsx
@@ -1,13 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNave() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
