0bb6ec3c97c3842a10f4460f80185b663c6a10f6
negar <niazadeh.negar@gmail.com>
2026-06-24 18:20:03 +0330

    add AppLayout page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
