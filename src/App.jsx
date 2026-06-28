f2ec112ab6b4710a6e04b04ae0376e69edd84481
negar <niazadeh.negar@gmail.com>
2026-06-28 19:35:11 +0330

    feat : fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,33 @@ import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import { useEffect, useState } from "react";
 
+const BASE_URL = "http://localhost:9000";
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoding] = useState(false);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        setIsLoding(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        if (!res.ok) throw new Error("somthing went wrong");
+
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.log(err);
+        alert(err.message);
+      } finally {
+        setIsLoding(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Routes>
@@ -14,13 +39,12 @@ function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} >
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>cities</p>} />
 
-        <Route index element={<p>cities</p>}/>
-
-        <Route path="cities" element={<p>cities</p>}/>
-        <Route path="countries" element={<p>countries</p>}/>
-        <Route path="form" element={<p>form</p>}/>
+          <Route path="cities" element={<p>cities</p>} />
+          <Route path="countries" element={<p>countries</p>} />
+          <Route path="form" element={<p>form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
